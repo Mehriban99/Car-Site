d781c009ab87b22dbc3041042daa09009f937840
Mehriban99 <57294601+Mehriban99@users.noreply.github.com>
2020-01-22 23:35:16 +0400

    Add files via upload

---
+++ car/js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".toggle-top").click(function(){
+        $(".toggle-alt").toggle();
+    })
+    
+})
+
+$(document).ready(function(){
+    $(".toggle2-top").click(function(){
+        $(".toggle2-alt").toggle();
+    })
+    
+})
+
+$(document).ready(function(){
+    $(".toggle-top-media").click(function(){
+        $(".toggle-alt-media").toggle();
+    })
+    
+})
+
+$(document).ready(function(){
+    $("#barclick").click(function(){
+        $(".bar-alt").slideToggle();
+    })
+})
